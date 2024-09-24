044619ce7bfe72247ce35514feaf70e2dbcea4f2
Divyansh Jitpure <divyanshjit2022@gmail.com>
2024-09-24 22:54:02 +0530

    ui integration with api done

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,14 +1,16 @@
 import express from "express";
 import fetch from "node-fetch";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
 const PORT = process.env.PORT || 4000;
 
-app.get("/", async (req, res) => {
+app.get("/api", async (req, res) => {
   try {
     const apiurl = await fetch(
-      "https://ulvis.net/API/write/get?url=https://www.youtube.com/watch?v=fgahxcqfyJ4&list=PLMQwtTz2aDBOy5LBSbihi3Ews_01KaobY&index=5"
+      "https://ulvis.net/API/write/get?url=https://www.youtube.com/watch?v=F02iMCEEQWs&list=PLMQwtTz2aDBOy5LBSbihi3Ews_01KaobY&index=9"
     );
     const data = await apiurl.json(); // Assuming the API returns JSON
     const shurl = data.data.url; // Extract the short URL from the API response
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,23 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [shorl, setShorl] = useState(null);
+
+  const fetchShortUrl = async () => {
+    const response = await fetch("http://localhost:4000/api");
+    const data = await response.json();
+    setShorl(data.msg);
+  };
+
+  fetchShortUrl();
+
+  return (
+    <>
+      <p>
+        Shortened URL: <a href={shorl}>{shorl}</a>
+      </p>
+    </>
+  );
 }
 
 export default App;
