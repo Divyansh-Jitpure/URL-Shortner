f91fc846571b809756bde1c387d272ca1528dfe8
Divyansh Jitpure <divyanshjit2022@gmail.com>
2024-09-24 22:21:10 +0530

    api integration 1st stage done

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,17 +1,23 @@
 import express from "express";
+import fetch from "node-fetch";
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
 app.get("/", async (req, res) => {
-  const apiurl = await fetch(
-    "https://ulvis.net/API/write/get?url=https://www.youtube.com/watch?v=fgahxcqfyJ4&list=PLMQwtTz2aDBOy5LBSbihi3Ews_01KaobY&index=5"
-  );
-  const data = await response.json(); // Assuming the API returns JSON
-  const shurl = data.url; // Extract the short URL from the API response
+  try {
+    const apiurl = await fetch(
+      "https://ulvis.net/API/write/get?url=https://www.youtube.com/watch?v=fgahxcqfyJ4&list=PLMQwtTz2aDBOy5LBSbihi3Ews_01KaobY&index=5"
+    );
+    const data = await apiurl.json(); // Assuming the API returns JSON
+    const shurl = data.data.url; // Extract the short URL from the API response
 
-  res.send(shurl);
+    res.send({ msg: shurl });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Error fetching shortened URL");
+  }
 });
 
 app.listen(PORT, () => {
